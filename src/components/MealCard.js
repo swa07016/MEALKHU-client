b5fd4854ecc949fffa2bd84b6441007c3926a09e
swa07016 <swa07016@naver.com>
2020-09-11 00:59:53 +0900

    mealcard 이미지 로딩 test, 비율 고정

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -3,8 +3,7 @@ import {
   Card, 
   CardBody, 
   CardTitle, 
-  CardText, 
-  CardImg, 
+  CardText,
   Modal, 
   ModalHeader, 
   ModalBody, 
@@ -17,10 +16,7 @@ import './MealCard.css';
 const MealCard = (props) => {
   
   const [modal, setModal] = useState(false);
-  const [loaded, setLoaded] = useState(false);
   const toggleModal = () => setModal(!modal);
-  const handleImageLoaded = () => setLoaded(true);
-  const imageStyle = !loaded ? { display: "none" } : {};
 
   return (
     <>
@@ -30,45 +26,28 @@ const MealCard = (props) => {
         'boxShadow': '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
       }}>
         <div className="wrap">
-        
-            <div style={{
-              width: '100%',
-              height: '15rem',
-              color: '#940f0f',
-              backgroundColor: '#fff',
-              display: loaded ? 'none':'flex',
-              justifyContent:'center',
-              alignItems: 'center',
-              fontSize:'1.7rem',
-              
-            }}>
-            <strong>MEALKHU</strong>
-            </div>
-            
-        
-        
-        <CardImg
-        top width="100%" 
-        src={`https://cdn.jsdelivr.net/gh/swa07016/mealkhu-cdn${props.img}?${Date.now()}`}
-        alt="Card image"
-        onLoad={handleImageLoaded}
-        style={imageStyle}
-        />
-        <CardBody>
-          <CardTitle className="text-center"><strong>{props.name}</strong></CardTitle>
-          <CardText className="text-center">
-            <small>{props.menu != 'null' ? props.menu : props.type}</small><br/>
-            <small style={{color:'#940f0f'}}>{(props.price != 'null')? `${props.price}￦` : ''}</small>
-          </CardText>
-        </CardBody>
-      
+          <img style={{
+            width:'100%',
+            paddingTop:'100%',
+            backgroundImage: `url(https://cdn.jsdelivr.net/gh/swa07016/mealkhu-cdn${props.img}?${Date.now()})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition:'center'
+          }}  
+          />        
+          <CardBody>
+            <CardTitle className="text-center"><strong>{props.name}</strong></CardTitle>
+            <CardText className="text-center">
+              <small>{props.menu != 'null' ? props.menu : props.type}</small><br/>
+              <small style={{color:'#940f0f'}}>{(props.price != 'null')? `${props.price}￦` : ''}</small>
+            </CardText>
+          </CardBody>
         </div>
       </Card>
      
       <Modal size="lg" className="modalClass" isOpen={modal} toggle={toggleModal}>
         <ModalHeader toggle={toggleModal}>{props.name}</ModalHeader>
         <ModalBody>
-        
         주소
         <hr className="my-2"/>
         <div>{props.address} <a href={`https://map.kakao.com/link/to/${props.name},${props.latitude},${props.longitude}`} style={{textDecoration:'none'}}>길찾기</a></div> 
